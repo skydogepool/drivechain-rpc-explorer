5f32190e84554f50f129771e6d38556c6f6d4718
skydogepool <105862062+skydogepool@users.noreply.github.com>
2023-04-26 22:22:16 +0800

    Update coins.js

--- app/coins.js
+++ app/coins.js
@@ -1,9 +1,11 @@
 "use strict";
 
 const btc = require("./coins/btc.js");
+const skydoge = require("./coins/skydoge.js");
 
 module.exports = {
 	"BTC": btc,
+	"SKYDOGE": skydoge,
 
-	"coins":["BTC"]
+	"coins":["BTC","SKYDOGE"]
 };
